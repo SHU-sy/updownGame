0e76114abaa144f3843778fcfdf5d0ecf9117b1a
SHU-sy <jisu.kimjss@gmail.com>
2024-08-02 13:40:40 +0900

    add level, life, retry

--- updown.py
+++ updown.py
@@ -10,61 +10,66 @@
 
 import random
 
-# 맞히는 숫자 설정
-com_num = random.randrange(1, 101)
+# 초기 life 개수 설정
+initial_life = 5
 
-# life 개수 설정
-life = 5
-
-# level 설정
+# 전역 level 설정
 level = 1
+
 
 # 현재 위치
 def total(life, level):
-    print("life : {}", life)
-    print("level : {}", level)
+    print("life : {}".format(life))
+    print("level : {}".format(level))
+
 
 # 입력한 숫자 검증
-def number_check(peo_num, com_num, life, level):
+def number_check(peo_num, com_num, life):
+    global level  # 전역 level 변수 사용
     if peo_num == com_num:  # 추측한 숫자가 맞았을 경우
         print("성공!!!")
-        level = level + 1
-        return retry()
+        level += 1
+        return retry(initial_life)
 
     elif life == 0:  # life 를 다 잃었을 경우
         print("실패!!!")
-        return retry()
+        return retry(initial_life)
 
     else:  # 추측한 숫자가 틀렸을 경우
         if peo_num > com_num:
             print("down!")
         else:
             print("up!")
+        life -= 1
+        if life == 0:
+            print("실패!!!")
+            return retry(initial_life)
+        print("\nlife = {}".format(life))
+        return life
 
 
 # 재도전 할건지 묻는 함수
-def retry():
-    retry_answer = input("재도전 하시겠습니까? y/n")
+def retry(life):
+    retry_answer = input("재도전 하시겠습니까? y/n: ")
     if retry_answer == "y":
-        total(level, life)
-        return game(com_num, life)
+        total(life, level)
+        return game(life)
 
     elif retry_answer == "n":
-        total(level, life)
+        total(life, level)
         exit()
 
     else:
         print("다시 입력해주세요")
-        return retry()
+        return retry(initial_life)
 
 
-def game(com_num, life):
-    for i in range(1, life + 1):
-        peo_num = int(input("숫자를 입력해주세요 : "))
+# 게임 함수
+def game(life):
+    com_num = random.randrange(1, 101)  # 새로운 게임 시작 시 숫자 생성
+    while life > 0:
+        peo_num = int(input("숫자를 입력해주세요: "))
+        life = number_check(peo_num, com_num, life)
 
-        number_check(peo_num, com_num, life)
 
-        print("\nlife = {}".format(life - i))
-
-
-game(com_num, life)
+game(initial_life)
